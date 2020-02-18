f9621f0b50758c2e426e1faa61455dc991e5fa65
miguelplazasr <info@miguelplazasr.com>
2020-02-17 23:41:59 -0500

    Preparacion de todoapp

--- 03-todoapp/src/app/app.module.ts
+++ 03-todoapp/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodosListComponent } from './todo/todos-list/todos-list.component';
+import { TodosItemComponent } from './todo/todo-item/todos-item.component';
+import { TodosFooterComponent } from './todo/todo-footer/todos-footer.component';
+import { TodoAddComponent } from './todo/todo-add/todo-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    TodoComponent,
+    TodosListComponent,
+    TodosItemComponent,
+    TodosFooterComponent,
+    TodoAddComponent
   ],
   imports: [
     BrowserModule
---
+++ 03-todoapp/src/app/todo/todo-footer/todos-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todos-footer',
+  templateUrl: './todos-footer.component.html',
+  styles: []
+})
+export class TodosFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
