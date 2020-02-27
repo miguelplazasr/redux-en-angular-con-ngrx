ea08d948ca57e105921af2372ec00c8bfebc00f5
miguelplazasr <info@miguelplazasr.com>
2020-02-27 06:53:51 -0500

    Effects Users

--- 05-http-effects/src/app/app.module.ts
+++ 05-http-effects/src/app/app.module.ts
@@ -1,11 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
+// NgRx
+import {StoreModule} from '@ngrx/store';
+import {EffectsModule} from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import {appReducers} from './store/app.reducer';
+import {AuthEffects} from './store/effects';
+
+// Environmet
+import {environment} from '../environments/environment';
+
+// Rutas
 import { AppRouting } from './app.routing';
+
+
+// Modulos personalizados
 import { AppComponent } from './app.component';
 import {SharedModule} from './shared/shared.module';
 import {UsuariosModule} from './usuarios/usuarios.module';
-import {HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -15,10 +31,18 @@ import {HttpClientModule} from '@angular/common/http';
     BrowserModule,
     AppRouting,
     HttpClientModule,
+    StoreModule.forRoot( appReducers ),
+    EffectsModule.forRoot( AuthEffects ),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production
+    }),
     SharedModule,
     UsuariosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  private static StoreDevtoolsModule: any;
+}
--- 05-http-effects/src/app/usuarios/lista/lista.component.ts
+++ 05-http-effects/src/app/usuarios/lista/lista.component.ts
@@ -1,6 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import {UsuarioService} from '../../services/usuario.service';
+import {Component, OnInit} from '@angular/core';
+
 import {UsuarioModel} from '../../models/usuario.model';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../store/app.reducer';
+
+import * as usuariosActions from '../../store/actions';
+import {Subscription} from 'rxjs';
+
 
 @Component({
   selector: 'app-lista',
@@ -10,13 +16,30 @@ import {UsuarioModel} from '../../models/usuario.model';
 export class ListaComponent implements OnInit {
 
   usuarios: UsuarioModel[] = [];
+  loading: boolean;
+  error: any;
+  subscription: Subscription = new Subscription();
 
   constructor(
-    private usuarioService: UsuarioService
-  ) { }
+    //private usuarioService: UsuarioService ToDo: Nota: Se comneta esta linea porque con Redux no se ustiliza el servicio en este punto
+    // ToDo: 10. Importar el store en los componentes
+    private store: Store<AppState>
+  ) {
+  }
 
   ngOnInit() {
-    this.usuarioService.getUsers().subscribe( (data: UsuarioModel[]) => this.usuarios = data);
+    // ToDo: Nota: Se comneta esta linea porque con Redux no se ustiliza el servicio en este punto
+    //this.usuarioService.getUsers().subscribe( (data: UsuarioModel[]) => this.usuarios = data);
+
+    this.store.dispatch(new usuariosActions.CargarUsuarios());
+
+    this.subscription = this.store.select('usuarios')
+      .subscribe( data => {
+        this.usuarios = data.users;
+        this.loading = data.loading;
+        this.error = data.error;
+      } );
+
   }
 
 }
